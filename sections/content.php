d833189d8fe727e2c4a155005fb10823639343ce
danielkchr <daniekchr@gmail.com>
2020-01-18 12:08:55 +1100

    added margin for content section

--- sections/content.php
+++ sections/content.php
@@ -6,25 +6,26 @@
 */ 
     $content_page = get_theme_mod('preschool_and_kindergarten_content_page');
         
-        if($content_page){
+	if($content_page)
+	{
+		$content_query = new WP_Query( array( 
+			
+			'p' => $content_page,
+			'post_type' => 'page'
 
-	        $content_query = new WP_Query( array( 
-	        	
-	        	'p' => $content_page,
-	        	'post_type' => 'page'
-
-	        	));
-        
-		        if($content_query->have_posts()){ $content_query->the_post();?>
-			        <section class="welcome">
-				        <div class="container">
+			));
+	
+			if($content_query->have_posts())
+			{ 
+				$content_query->the_post();
+				?>
+					<section class="home-content">
+						<div class="container">
 							<?php the_content(); ?>
 						</div>
 					</section>	
-			    <?php 
-
-			    }
-			    
-			    wp_reset_postdata(); 
-	    } 
+				<?php 
+			}
+			wp_reset_postdata(); 
+	} 
 ?>
